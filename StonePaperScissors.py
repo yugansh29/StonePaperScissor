30fae145d7415da44b52767239514d5868275733
Yugansh Kumar <yugansh2910@gmail.com>
2023-10-22 14:47:18 +0530

    Update StonePaperScissors.py

--- StonePaperScissors.py
+++ StonePaperScissors.py
@@ -1,32 +1,28 @@
 import random
-
-choices={"Stone":"🪨","Paper":"📄","Scissor":"✂️"}
-computer=random.choice(list(choices))
-_= None
-while (_ not in choices):
-    _ = input("Enter your choice Stone,Paper,Scissor ").capitalize()
-print ("You choose "+ _ )
-print("Computer Choose "+computer)
-
-if (_== computer):
-    print("It's a Tie !!!")
-elif(_=="Scissor" and computer=="Paper" ):
-    print ("You Won!!!")
-elif(_=="Paper" and computer=="Scissor" ):
-    print ("You lost!!!")
-elif(_=="Stone" and computer=="Scissor" ):
-    print ("You Won!!!")
-elif(_=="Scissor" and computer=="Stone" ):
-    print ("You Lost!!!")
-elif(_=="Stone" and computer=="Paper" ):
-    print ("You Lost!!!")
-elif(_=="Paper" and computer=="Stone" ):
-    print ("You Won!!!")
-
-
+while True:
+    choices={"Stone":"🪨","Paper":"📄","Scissor":"✂️"}
+    computer=random.choice(list(choices))
+    _= None
+    while (_ not in choices):
+        _ = input("Enter your choice Stone,Paper,Scissor ").capitalize()
+    print ("You choose "+ _ )
+    print("Computer Choose "+computer )   
+    if (_== computer):
+        print("It's a Tie !!!")
+    elif(_=="Scissor" and computer=="Paper" ):
+        print ("You Won!!!")
+    elif(_=="Paper" and computer=="Scissor" ):
+        print ("You lost!!!")
+    elif(_=="Stone" and computer=="Scissor" ):
+        print ("You Won!!!")
+    elif(_=="Scissor" and computer=="Stone" ):
+        print ("You Lost!!!")
+    elif(_=="Stone" and computer=="Paper" ):
+        print ("You Lost!!!")
+    elif(_=="Paper" and computer=="Stone" ):
+        print ("You Won!!!")
     
-
-    
-       
-
-    
+    playAgain=input("Would you like to try again(yes/no)").lower()
+    if playAgain =="no":
+        break
+print ("bye")
